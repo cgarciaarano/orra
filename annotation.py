62851e6e2ee60d42b65247a4005a88c8c15ac5b1
cgarciaarano <cgarciaarano@gmail.com>
2015-11-17 09:41:58 +0100

    New class Game and added methods

--- annotation.py
+++ annotation.py
@@ -1,34 +1,83 @@
 from datetime import datetime 
 
-class Annotation(object):
-    def __init__(self):
+class Game(object):
+    def __init__(self, d = None):
 
-        self.date = None
-        self.opponent = ''
-        self.player = ''
-        self.points = 0
-        self.play = False
+        if not d:
+            self.date = None
+            self.opponent = ''
+            self.result = None
+            self.players = []
+        else:
+            self.from_dict(d)
 
     def __str__(self):
         return '{0}'.format(self.get_json())
 
-    def get_json(self):
-        return {
+    def add_player(self, Player):
+        self.players.append(Player)
+
+    def from_dict(self, d):
+        self.date = d['date']
+        self.opponent = d['opponent']
+        self.result = d['result']
+        self.players = []
+
+        for player in d['players']:
+            self.add_player(Player(player))
+
+    def get_influx_json(self):
+        game = []
+        for player in self.players:
+            game.append({
                 "measurement": "points",
                 "tags": {
                     "opponent": self.opponent,
-                    "player": self.player,
-                    "play": self.play
+                    "player": player.player,
+                    "play": player.play,
+                    "result": self.result,
                 },
                 "time": self.date,
                 "fields": {
-                    "value": self.points
+                    "value": player.points
                 }
-            }
+            })
+        return game
 
-    def from_dict(self, date, opponent, data):
-        self.date = date
-        self.opponent = opponent
+    def get_json(self):
+        game = {
+            'date': self.date,
+            'opponent' : self.opponent,
+            'result' : self.result,
+            'players' : [],
+        }
+
+        for player in self.players:
+            game['players'].append(player.get_json())
+
+        return game
+
+class Player(object):
+    def __init__(self, d = None):
+
+        if not d:
+            self.player = ''
+            self.points = 0
+            self.play = False
+        else:
+            self.from_dict(d)
+
+    def __str__(self):
+        return 'Player: {0} Points: {1} Play: {2}'.format(self.player, self.points, self.play)
+
+    def from_dict(self, data):
         self.player = data['player']
         self.points = data['points']
         self.play = data['play']
+
+    def get_json(self):
+        return {
+            "player": self.player,
+            "play": self.play,
+            "points": self.points,
+            }
--- parser.py
+++ parser.py
@@ -8,36 +8,36 @@ from datetime import datetime
 import annotation
 
 def parse_file(filename):
-	with open(filename, 'r') as csvfile:
-		reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-		
-		games = []
-		dates = [date for date in reader.next() if date]
-		result = [result for result in reader.next() if result]
-		opponents = [opponent for opponent in reader.next() if opponent]
-		
-		for i,date in enumerate(dates):
-			game = {'date' : datetime.strptime(date,'%d/%m/%Y') , 'opponent' : opponents[i], 'result' : result[i], 'players' : []}
-			games.append(game)
+    with open(filename, 'r') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        
+        games = []
+        dates = [date for date in reader.next() if date]
+        result = [result for result in reader.next() if result]
+        opponents = [opponent for opponent in reader.next() if opponent]
+        
+        for i,date in enumerate(dates):
+            game = {'date' : datetime.strptime(date,'%d/%m/%Y') , 'opponent' : opponents[i], 'result' : result[i], 'players' : []}
+            games.append(game)
 
-		for row in reader:
-			player = row[0]
-			if not player:
-				continue
-			for i,column in enumerate(row[1:]):
+        for row in reader:
+            player = row[0]
+            if not player:
+                continue
+            for i,column in enumerate(row[1:]):
 
-				if column is '-' or not column:
-					points = 0
-					play = False
-				else:
-					play = True
-					points = column
+                if column is '-' or not column:
+                    points = 0
+                    play = False
+                else:
+                    play = True
+                    points = column
 
-				games[i]['players'].append({'player': player, 'points': int(points), 'play': bool(play) })
+                games[i]['players'].append({'player': player, 'points': int(points), 'play': bool(play) })
 
-		return games
+        return games
 
-			
+            
 if __name__ == '__main__':
 
         if len(sys.argv) < 2:
@@ -45,23 +45,23 @@ if __name__ == '__main__':
         else:
                 games = parse_file(sys.argv[1])
 
-
-                print('{0}'.format(max([ player['points'] for game in games for player in game['players']  ])))
+                import pprint
+                for game in games:
+                    pprint.pprint(annotation.Game(game).get_json())
 
                 influx = False
                 if influx:
-                	from influxdb import InfluxDBClient
+                    json_games = []
+                    for game in games:
 
-	                json_game = []
-	                for game in games:
-	                        for player_data in game['players']:
-	                                an = annotation.Annotation()
-	                                an.from_dict(game['date'], game['opponent'], player_data)
+                        json_game = annotation.Game(game).get_influx_json()
+                        json_games.extend(json_game)
+                    import pprint
+                    pprint.pprint(json_games)
 
-	                                json_game.append(an.get_json())
-
-	                try:
-	                        client = InfluxDBClient('localhost', 8086, 'root', 'root', 'orra')
-	                        client.write_points(json_game)
-	                except InfluxDBClientError as e:
-	                        print e.content
+                    try:
+                            from influxdb import InfluxDBClient
+                            client = InfluxDBClient('localhost', 8086, 'root', 'root', 'orra')
+                            client.write_points(json_game)
+                    except InfluxDBClientError as e:
+                            print e.content
